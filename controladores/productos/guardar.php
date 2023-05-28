1b1329186083914ca87a653f27839abdf9b3c793
dannycornelio <licdannycornelio@gmail.com>
2023-05-28 11:35:05 -0600

    creacion del archivo navbar

--- controladores/productos/guardar.php
+++ controladores/productos/guardar.php
@@ -20,27 +20,30 @@ try {
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Resultados</title>
 </head>
+
 <body>
     <div class="container">
         <div class="row">
             <div class="col-lg-6">
                 <?php if($resultado): ?>
-                    <div class="alert alert-success" role="alert">
-                        Guardado exitosamente!
-                    </div>
+                <div class="alert alert-success" role="alert">
+                    Guardado exitosamente!
+                </div>
                 <?php else :?>
-                    <div class="alert alert-danger" role="alert">
-                        Ocurrió un error: <?= $error ?>
-                    </div>
+                <div class="alert alert-danger" role="alert">
+                    Ocurrió un error: <?= $error ?>
+                </div>
                 <?php endif ?>
-              
+
             </div>
         </div>
         <div class="row">
@@ -50,4 +53,5 @@ try {
         </div>
     </div>
 </body>
+
 </html>
